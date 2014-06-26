c5af9fc7a4d9ad4745e9147cc3d76442b62d260e
Evan Jones <ej@evanjones.ca>
2014-06-26 17:05:18 -0400

    Second demo

---
+++ 2_manual/demo.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+rm -f exe
+git co exe.c hello.h
+echo "CLEAN BUILD (exe gets built):"
+set -x
+make
+./exe
+set +x
+
+echo
+echo "REBUILD with nothing updated (nothing gets built):"
+set -x
+make
+set +x
+
+sleep 1  # Mac OS X HFS+ timestamps have 1s resolution
+echo
+echo "UPDATE exe.c (rebuilt and prints GOODBYE):"
+set -x
+perl -pi -e 's/hello from/GOODBYE from/;' exe.c
+make
+./exe
+set +x
+
+sleep 1  # Mac OS X HFS+ timestamps have 1s resolution
+echo
+echo "UPDATE hello.h (IS rebuilt; prints the CORRRECT output):"
+set -x
+perl -pi -e 's/says hello/says GOODBYE/;' hello.h
+make
+./exe
+set +x
