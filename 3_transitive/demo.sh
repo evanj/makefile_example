2db525d2366a64e1e650a5230c9d9a928ca28c6f
Evan Jones <ej@evanjones.ca>
2014-06-27 14:35:13 -0400

    Transitive includes demo.

---
+++ 3_transitive/demo.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+git co other.h hello.h
+make clean
+echo "CLEAN BUILD (exe gets built):"
+set -x
+make
+./exe
+set +x
+
+echo
+echo "REBUILD with nothing updated (nothing gets built):"
+set -x
+make
+set +x
+
+sleep 1  # Mac OS X HFS+ timestamps have 1s resolution
+echo
+echo "UPDATE hello.h to include other.h (IS rebuilt; prints the CORRRECT output):"
+set -x
+cp hello2.h hello.h
+make
+./exe
+set +x
+
+
+sleep 1  # Mac OS X HFS+ timestamps have 1s resolution
+echo
+echo "UPDATE other.h (rebuilt and prints UPDATED):"
+set -x
+perl -pi -e 's/"printOther()/"printOther() UPDATED/;' other.h
+make
+./exe
+set +x
